9a107e82b0bfe018f8d9b56bdafabf540585623e
guillermoleonq <guillermoleonq@yahoo.com>
2024-09-18 21:11:40 -0500

    Second Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
-import { MyFirstComponent } from './components/MyFirstComponent';;
+import { MyFirstComponent } from './components/MyFirstComponent';
+import { SecondComponent } from './components/SecondComponent';
+;
 function App() {
    return (
     <>
      <h1>Fundamentos de React</h1>
      <MyFirstComponent />
+     <SecondComponent />
     </>
   )
 }
--- src/components/MyFirstComponent.jsx
+++ src/components/MyFirstComponent.jsx
@@ -1,15 +1,42 @@
 
 export const MyFirstComponent = () => {
-  return (
+const name = "Guillermo Leon";
+const github_profile = "https://gitub.com/guillermoleonq";
+const student ={
+  name1: "Miguel",
+  last_name: "Leon",
+  mobile: "3001234567",
+  linkedind_profile: ""
+} 
+
+return (
     <>
       <div>MyFirstComponent</div>
-      <h1>Temmas de React</h1>
-      <ul>
-        <li>Componentes</li>
-        <li>Eventos</li>
-        <li>Estados Hooks</li>
-        <li>Props</li>
-      </ul>
+      <div className="container bg-info-subtle mt-4">
+        <h1>Temmas de React</h1>
+        <ul>
+          <li>Componentes</li>
+          <li>Eventos</li>
+          <li>Estados Hooks</li>
+          <li>Props</li>
+        </ul>
+      </div>
+      <div className="container bg-warning-subtle py-2">
+        <h1>Datos del Docente</h1>
+        <p>Nombre: <strong>{name }</strong></p>
+        <p>GitHub: <a href="">{ github_profile }</a> </p>
+      </div>
+      <div className="container bg-success-subtle py-2">
+        <h1>Datos del Estudiante</h1>
+        <p>Nombre: <strong>{ student.name }</strong></p>
+        <p>Apellido: { student.last_name } </p>
+        <p>Celular: { student.mobile } </p>
+        <p>LinkedIn: <a href={ student.linkedin_profile }>{ student.linkedin_profile }</a> </p>
+      </div>
+      <div className="container mt-2 pt-2">
+        <h1>Objeto Completo</h1>
+        <pre>{JSON.stringify(student)} </pre>
+      </div>
     </>
   )
 }
