d597b116400aed619138f91b4c4a0eaabfc4ac59
guillermoleonq <guillermoleonq@yahoo.com>
2024-09-22 11:00:06 -0500

    ThirdComponet: props - ChildComponet: hook (start)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import './App.css';
-import { MyFirstComponent } from './components/MyFirstComponent';
-import { SecondComponent } from './components/SecondComponent';
+import { useState } from 'react';
+// import { MyFirstComponent } from './components/MyFirstComponent';
+// import { SecondComponent } from './components/SecondComponent';
+import { ThirdComponent } from './components/ThirdComponent';
+import { Child } from './components/Child';
 
 function App() {
+
+  const [name,setName] = useState ("Lina");
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Fundamentos de React</h1>
-        <MyFirstComponent />
-        <SecondComponent />
+        <hr className='white-hr' />
+        {/* <MyFirstComponent /> */}
+        <hr className='white-hr' />
+        {/* <SecondComponent /> */}
+        <hr className='white-hr' />
+        <ThirdComponent 
+          name="Guillermo"
+          last_name="Leon"
+          age="30"
+        /> 
+        <hr className='white-hr' />
+        <Child 
+          name = {name}
+          setName={setName} 
+        />
+        <hr className='white-hr' />
       </header>
     </div>
   )
 };
 
 export default App
-
--- src/components/SecondComponent.jsx
+++ src/components/SecondComponent.jsx
@@ -9,19 +9,25 @@ export const SecondComponent = () => {
       "Harry Potter y el misterio del Príncipe",
       "Harry Potter y las Reliquias de la Muerte",
       "Animales Fantásticos y dónde encontrarlos",
-      "Otro libro"
+      "Otro libro",
+      "De nuevo otro libro"
     ];
- 
+    
     return (
       <>
         <div className="container mt-2 py-2">
           <h1>SecondComponent</h1>
           <div>
             <ul>
-              {
-                books.map((book, index) => {
-                  return <li key={ index }> { book } </li>
-                })
+              {books.length >= 1 ?
+                (
+                  books.map((book, index) => {
+                    return <li key={ index }> { book } </li>
+                  })
+                ) :
+                (
+                  <p>No existen libros</p>
+                )
               }
             </ul>
     
